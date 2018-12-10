11e7c496fe689c5f8a559e6b212f8063c9f3fb9b
Hudson Graham <hudson.graham@gmail.com>
2018-12-10 14:43:13 +1000

    Clean up comments add logging flag

--- src/app/trending-bets/racing-bet-list/racing-bet-list.component.ts
+++ src/app/trending-bets/racing-bet-list/racing-bet-list.component.ts
@@ -8,66 +8,11 @@ import {
   getIsRacingBetsLoading,
   getRacingBetsState
 } from '../../reducers/index';
-// import {
-//   trigger,
-//   state,
-//   style,
-//   animate,
-//   transition,
-//   AnimationEvent
-// } from '@angular/animations';
 import { RacingBet } from '../models/trending-bets';
 
 @Component({
   selector: 'racing-bet-list',
-  animations: [
-    // trigger('openClose', [
-    //   state('open', style({
-    //     backgroundColor: 'yellow'
-    //   })),
-    //   state('closed', style({
-    //     backgroundColor: 'green'
-    //   })),
-    //   transition('* => *', [
-    //     animate('1000ms 500ms ease-in', style({ backgroundColor: 'blue' }))
-    //   ]),
-    // ]),
-    // trigger('openClose', [
-    //   state('open', style({
-    //     height: '200px',
-    //     opacity: 1,
-    //     backgroundColor: 'yellow'
-    //   })),
-    //   state('closed', style({
-    //     height: '100px',
-    //     opacity: 0.5,
-    //     backgroundColor: 'green'
-    //   })),
-    //   transition('open => closed', [
-    //     animate('1s')
-    //   ]),
-    //   transition('closed => open', [
-    //     animate('0.5s')
-    //   ]),
-    //   transition('* => closed', [
-    //     animate('1s')
-    //   ]),
-    //   transition('* => open', [
-    //     animate('0.5s')
-    //   ]),
-    //   transition('open <=> closed', [
-    //     animate('0.5s')
-    //   ]),
-    //   transition('* => open', [
-    //     animate('1s',
-    //       style({ opacity: '*' }),
-    //     ),
-    //   ]),
-    //   transition('* => *', [
-    //     animate('1s')
-    //   ]),
-    // ]),
-  ],
+  animations: [],
   templateUrl: './racing-bet-list.component.html',
   styleUrls: ['./racing-bet-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -76,12 +21,7 @@ export class RacingBetListComponent implements OnInit {
   racingBets$: Observable<RacingBet[]>;
   isLoading$: Observable<boolean>;
 
-  // isOpen = false;
-
-  // toggle() {
-  //   console.log("toggle clicked");
-  //   this.isOpen = !this.isOpen;
-  // }
+  logging = false;
 
   constructor(private store: Store<State>) { }
 
@@ -101,25 +41,4 @@ export class RacingBetListComponent implements OnInit {
     // console.log(" >>>>> " + temp);
     return temp;
   }
-
-  // onAnimationEvent(event: AnimationEvent) {
-
-  //   // openClose is trigger name in this example
-  //   console.warn(`Animation Trigger: ${event.triggerName}`);
-
-  //   // phaseName is start or done
-  //   console.warn(`Phase: ${event.phaseName}`);
-
-  //   // in our example, totalTime is 1000 or 1 second
-  //   console.warn(`Total time: ${event.totalTime}`);
-
-  //   // in our example, fromState is either open or closed
-  //   console.warn(`From: ${event.fromState}`);
-
-  //   // in our example, toState either open or closed
-  //   console.warn(`To: ${event.toState}`);
-
-  //   // the HTML element itself, the button in this case
-  //   console.warn(`Element: ${event.element}`);
-  // }
 }
--- src/app/trending-bets/racing-bet/racing-bet.component.ts
+++ src/app/trending-bets/racing-bet/racing-bet.component.ts
@@ -20,7 +20,7 @@ import {
         style({
           backgroundColor: 'yellow'
         }),
-        animate('0.5s')
+        animate('2.5s')
       ]),
     ]),
   ],
@@ -33,25 +33,16 @@ export class RacingBetComponent {
   toggle() {
     this.isUpdated = !this.isUpdated;
   }
-
+  @Input() logging: boolean = false;
   onAnimationEvent(event: AnimationEvent) {
-
-    // openClose is trigger name in this example
+    if (!this.logging) {
+      return
+    }
     console.warn(`Animation Trigger: ${event.triggerName}`);
-
-    // phaseName is start or done
     console.warn(`Phase: ${event.phaseName}`);
-
-    // in our example, totalTime is 1000 or 1 second
     console.warn(`Total time: ${event.totalTime}`);
-
-    // in our example, fromState is either open or closed
     console.warn(`From: ${event.fromState}`);
-
-    // in our example, toState either open or closed
     console.warn(`To: ${event.toState}`);
-
-    // the HTML element itself, the button in this case
     console.warn(`Element: ${event.element}`);
   }
 }
